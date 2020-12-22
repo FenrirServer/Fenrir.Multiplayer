962074121825c1b6adf5d300bb57657b35b52b68
lumanskiy <splatt@splatt.ru>
2020-12-21 23:14:34 -0800

    Message writer tests

--- Fenrir.Multiplayer.Tests/Unit/LiteNetProtocol/LiteNetMessageWriterTests.cs
+++ Fenrir.Multiplayer.Tests/Unit/LiteNetProtocol/LiteNetMessageWriterTests.cs
@@ -47,6 +47,65 @@ namespace Fenrir.Multiplayer.Tests.Unit.LiteNetProtocol
             Assert.AreEqual("test", testEvent.Value);
         }
 
+        [TestMethod]
+        public void LiteNetMessageWriter_WriteMessage_WritesRequest()
+        {
+            var typeHashMap = new TypeHashMap();
+            var serializationProvider = new SerializationProvider();
+            var messageWriter = new LiteNetMessageWriter(serializationProvider, typeHashMap, new EventBasedLogger(), new RecyclableObjectPool<ByteStreamWriter>());
+
+            var netDataWriter = new NetDataWriter();
+            var messageWrapper = new MessageWrapper() { MessageType = MessageType.Request, RequestId = 123, MessageData = new TestRequest() { Value = "test" }, IsEncrypted = true };
+            messageWriter.WriteMessage(netDataWriter, messageWrapper);
+
+            // Validate 
+            // Get hash
+            var netDataReader = new NetDataReader(netDataWriter.Data);
+            Assert.AreEqual(typeHashMap.GetTypeHash<TestRequest>(), netDataReader.GetULong()); // [ulong] type hash
+
+            // Get flags
+            ushort flags = netDataReader.GetUShort();
+            MessageFlags messageFlags = (MessageFlags)flags; // Flags enum
+            ushort requestId = (ushort)(flags >> 4); // Request id
+
+            Assert.IsTrue(messageFlags.HasFlag(MessageFlags.Encrypted));
+            Assert.AreEqual(123, requestId);
+
+            var testRequest = serializationProvider.Deserialize<TestRequest>(new ByteStreamReader(netDataReader));
+
+            Assert.AreEqual("test", testRequest.Value);
+        }
+
+
+        [TestMethod]
+        public void LiteNetMessageWriter_WriteMessage_WritesResponse()
+        {
+            var typeHashMap = new TypeHashMap();
+            var serializationProvider = new SerializationProvider();
+            var messageWriter = new LiteNetMessageWriter(serializationProvider, typeHashMap, new EventBasedLogger(), new RecyclableObjectPool<ByteStreamWriter>());
+
+            var netDataWriter = new NetDataWriter();
+            var messageWrapper = new MessageWrapper() { MessageType = MessageType.Response, RequestId = 123, MessageData = new TestResponse() { Value = "test" }, IsEncrypted = true };
+            messageWriter.WriteMessage(netDataWriter, messageWrapper);
+
+            // Validate 
+            // Get hash
+            var netDataReader = new NetDataReader(netDataWriter.Data);
+            Assert.AreEqual(typeHashMap.GetTypeHash<TestResponse>(), netDataReader.GetULong()); // [ulong] type hash
+
+            // Get flags
+            ushort flags = netDataReader.GetUShort();
+            MessageFlags messageFlags = (MessageFlags)flags; // Flags enum
+            ushort requestId = (ushort)(flags >> 4); // Request id
+
+            Assert.IsTrue(messageFlags.HasFlag(MessageFlags.Encrypted));
+            Assert.AreEqual(123, requestId);
+
+            var testResponse = serializationProvider.Deserialize<TestResponse>(new ByteStreamReader(netDataReader));
+
+            Assert.AreEqual("test", testResponse.Value);
+        }
+
         #region Test Fixtures
         class TestEvent : IEvent, IByteStreamSerializable
         {
